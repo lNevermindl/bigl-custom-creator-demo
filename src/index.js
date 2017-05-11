22a157ad0c9b75dd61c4502c223df8d71139f3af
Roman Kiyanitsa <r.kiyanitsa@smartweb.com.ua>
2017-05-11 13:03:00 +0300

    updated demo for v1.1.0

--- src/index.js
+++ src/index.js
@@ -3,5 +3,83 @@ import biglCustomCreator from 'bigl-custom-creator';
 import 'bigl-custom-creator/dist/index.min.css';
 
 const node = document.getElementById('app');
+const presetTemplates = [
+    [
+        {
+            column: 1,
+            row: 1,
+        },
+        {
+            column: 2,
+            row: 1,
+        },
+        {
+            column: 3,
+            row: 1,
+        },
+        {
+            column: 4,
+            row: 1,
+        },
+    ],
+    [
+        {
+            column: [1, 2],
+            row: 1,
+        },
+        {
+            column: 3,
+            row: 1,
+        },
+        {
+            column: 4,
+            row: 1,
+        },
+    ],
+    [
+        {
+            column: 1,
+            row: 1,
+        },
+        {
+            column: [2, 3],
+            row: 1,
+        },
+        {
+            column: 4,
+            row: 1,
+        },
+    ],
+    [
+        {
+            column: 1,
+            row: 1,
+        },
+        {
+            column: 2,
+            row: 1,
+        },
+        {
+            column: [3, 4],
+            row: 1,
+        },
+    ],
+    [
+        {
+            column: [1, 2],
+            row: 1,
+        },
+        {
+            column: [3, 4],
+            row: 1,
+        },
+    ],
+    [
+        {
+            column: [1, 2, 3, 4],
+            row: 1,
+        },
+    ],
+];
 
-biglCustomCreator(node);
+biglCustomCreator(node, { presetTemplates: presetTemplates });
