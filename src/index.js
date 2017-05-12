cb08622dda7833c1839eba925487d87d431a9799
Roman Kiyanitsa <r.kiyanitsa@smartweb.com.ua>
2017-05-12 14:20:34 +0300

    updated to v1.1.3

--- src/index.js
+++ src/index.js
@@ -5,81 +5,35 @@ import 'bigl-custom-creator/dist/index.min.css';
 const node = document.getElementById('app');
 const presetTemplates = [
     [
-        {
-            column: 1,
-            row: 1,
-        },
-        {
-            column: 2,
-            row: 1,
-        },
-        {
-            column: 3,
-            row: 1,
-        },
-        {
-            column: 4,
-            row: 1,
-        },
+        { column: 1, row: 1 },
+        { column: 2, row: 1 },
+        { column: 3, row: 1 },
+        { column: 4, row: 1 }
     ],
     [
-        {
-            column: [1, 2],
-            row: 1,
-        },
-        {
-            column: 3,
-            row: 1,
-        },
-        {
-            column: 4,
-            row: 1,
-        },
+        { column: [1, 2], row: 1 },
+        { column: 3, row: 1 },
+        { column: 4, row: 1 }
     ],
     [
-        {
-            column: 1,
-            row: 1,
-        },
-        {
-            column: [2, 3],
-            row: 1,
-        },
-        {
-            column: 4,
-            row: 1,
-        },
+        { column: 1, row: 1 },
+        { column: [2, 3], row: 1 },
+        { column: 4, row: 1 }
     ],
     [
-        {
-            column: 1,
-            row: 1,
-        },
-        {
-            column: 2,
-            row: 1,
-        },
-        {
-            column: [3, 4],
-            row: 1,
-        },
+        { column: 1, row: 1 },
+        { column: 2, row: 1 },
+        { column: [3, 4], row: 1 }
     ],
     [
-        {
-            column: [1, 2],
-            row: 1,
-        },
-        {
-            column: [3, 4],
-            row: 1,
-        },
+        { column: [1, 2], row: 1 },
+        { column: [3, 4], row: 1 }
     ],
     [
-        {
-            column: [1, 2, 3, 4],
-            row: 1,
-        },
-    ],
+        { column: [1, 2, 3, 4], row: 1 }
+    ]
 ];
+const importJSON = null;
+const onExportJSON = null;
 
-biglCustomCreator(node, { presetTemplates: presetTemplates });
+biglCustomCreator(node, { presetTemplates: presetTemplates, importJSON: importJSON, onExportJSON: onExportJSON });
